c6803be644abc3003a90f3c30efd6703f2b1ecf6
Tord Natlandsmyr <tordna@protonmail.com>
2021-11-11 22:25:34 +0100

    Fucked up git, sowi

---
+++ RobotBuddy/RobotBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RobotBuddyApp.swift
+//  RobotBuddy
+//
+//  Created by Tord Natlandsmyr on 03/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct RobotBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
